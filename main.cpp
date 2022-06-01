29e444cec6ec289fb7a68f6d46946ba74a44ef3f
RangAl <71291523+Rang-Alv@users.noreply.github.com>
2022-06-01 12:28:40 +0100

    function call

--- main.cpp
+++ main.cpp
@@ -26,6 +26,10 @@ int main() {
     vector<Product> products;
     vector<Category> categories;
 
+    //Category category;
+
+    //cout << category.trunStr("God Help Our Souls");
+
     // choose between User and Admin
     User *user = new User();
     Admin admin;
